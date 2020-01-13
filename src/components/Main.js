0b5f06e545168e7f4d75ec75c33f462a4cd0786d
Ferdy Macleod <ferdymacleod21@gmail.com>
2020-01-14 08:41:19 +1000

    added landing page

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,28 @@ import GitLogo from "../images/github.png"
 import TwitLogo from "../images/twitter.png"
 import LinkLogo from "../images/linkedin.png"
 
+const Landing = () => {
+    return (
+        <div className="landing-div">
+            <div className="title-div">
+                <h1 className="title">Ferdy Macleod</h1>
+                <h2 className="sub-title">Full Stack Developer</h2>
+            </div>
+            <p className="intro-text">I make <span>effective</span> <span>responsive</span> and <span>dynamic</span> websites</p>
+        </div>
+    )
+}
+
 const Main = () => {
     return(
         <>
+            <Landing />
             <div className="main">
                 <div className="profile-div">
                     <img src={ProfilePic} alt="" className="profile-pic"/>
-                    <div className="title-div">
-                        <h1 className="title">Ferdy Macleod</h1>
-                        <h2 className="sub-title">Full Stack Developer</h2>
+                    <div className="profile-title-div">
+                        <h1 className="profile-title">Ferdy Macleod</h1>
+                        <h2 className="profile-sub-title">Full Stack Developer</h2>
                     </div>
                     <div className="social-icons">
                         <img src={GitLogo} alt="Link to my github"/>
