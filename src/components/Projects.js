666c78c1b0c52595d728996d89bbdfe9bb533615
Ferdy Macleod <ferdymacleod21@gmail.com>
2020-02-04 16:14:10 +1000

    added dynamic rendering of social icons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,15 @@
 import React from "react"
 import Navbar from "./Navbar"
 import GitLogo from "../images/github.png"
+import LinkedinLogo from "../images/linkedin.png"
+import TwitterLogo from "../images/twitter.png"
 import Pic from "../images/pic.jpeg"
 
 const ExampleProject = {
     title: "VisualSort",
     images: [GitLogo, Pic],
     text: 'this is just example placeholder text im using in order to make an example',
-    techStack: ['Link to react logo', 'link to js logo', 'you get the point']
+    techStack: [GitLogo, LinkedinLogo, TwitterLogo]
 }
 
 const Projects = () => {
@@ -26,9 +28,17 @@ const ProjectCard = (props) => {
     return (
         <div className="project-card">
             <img src={props.example.images[0]} alt="" className="project-img"/>
-            <div className="img-selector-wrapper"></div>
-            <p className="project-text">{props.example.text}</p>
-            <div className="tech-stack-icons-wrapper"></div>
+            <div className="project-main-content-wrapper">
+                <div className="img-selector-wrapper">
+                    <div className="img-selector-button"></div>
+                </div>
+                <p className="project-text">{props.example.text}</p>
+                <div className="tech-stack-icons-wrapper">
+                    {props.example.techStack.map(icon =>
+                        <img src={icon} className="tech-stack-icon"/>
+                    )}
+                </div>
+            </div>
             <div className="project-buttons-wrapper">
                 <button className="project-button project-button1"></button>
                 <button className="project-button project-button2"></button>
